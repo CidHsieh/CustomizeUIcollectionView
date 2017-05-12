0be51591e2a8f548ac3e1f4fe8bed2003662ec2d
Cid Hsieh <topkofaxion7671@hotmail.com>
2017-05-12 17:03:48 +0800

    initial commit

---
+++ CustomizeUIcollectionView/CustomizeUIcollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomizeUIcollectionView
+//
+//  Created by Cid Hsieh on 2017/5/12.
+//  Copyright © 2017年 Cid Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
