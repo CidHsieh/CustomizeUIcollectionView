98dca038fd8a9c190a4dc97b123ee384a1c4285b
Cid Hsieh <topkofaxion7671@hotmail.com>
2017-05-13 15:15:04 +0800

    加入導航至ALPHA camp功能

--- CustomizeUIcollectionView/CustomizeUIcollectionView/ViewController.swift
+++ CustomizeUIcollectionView/CustomizeUIcollectionView/ViewController.swift
@@ -7,16 +7,52 @@
 //
 
 import UIKit
+import CoreMotion
+import MapKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     var cellContent = ["顯示一個Alert View","顯示藍色，點擊後變成紅色，再次點擊又會是藍色","透過過core motion顯示使用者現在的步數，並且即時更新","開啟此App在IOS的設定頁面","打開google map或是web導航至Alpha camp","開啟信箱，並將標題填上測試信件"]
+    
+    var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        motionManager.deviceMotionUpdateInterval = 0.1
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceManager, error) in
+            let accelerationThrehold:Double = 1
+            let userAcceleration:CMAcceleration = deviceManager!.userAcceleration
+            
+            if let myData = deviceManager {
+                print(myData)
+            }
+            
+            if (fabs(userAcceleration.x) > accelerationThrehold) || (fabs(userAcceleration.y) > accelerationThrehold) || (fabs(userAcceleration.z) > accelerationThrehold) {
+                
+                print("")
+            }
+        }
+        
+        
         collectionView.delegate = self
         collectionView.dataSource = self
+    }
+    
+    //導航function，帶入經緯度，目的地名稱的參數
+    func navigation(lat:Float, lon:Float, placeName:String){
+        let latitude: CLLocationDegrees = CLLocationDegrees(lat)
+        let longitude:CLLocationDegrees = CLLocationDegrees(lon)
+        
+        let regineDistance:CLLocationDistance = 1000.0
+        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+        let reginSpan = MKCoordinateRegionMakeWithDistance(coordinates, regineDistance, regineDistance)
+        
+        let option = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: reginSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: reginSpan.span)]
+        let placeMark = MKPlacemark(coordinate: coordinates)
+        let mapItem = MKMapItem(placemark: placeMark)
+        mapItem.name = placeName
+        mapItem.openInMaps(launchOptions: option)
     }
 
 
@@ -45,6 +81,34 @@ extension ViewController: UICollectionViewDelegate {
                 cell.backgroundColor = UIColor.blue
                 print("456")
             }
+        //加入core motion功能
+        }else if indexPath.row == 2 {
+            
+        
+        //加入切換至設定功能
+        } else if indexPath.row == 3 {
+            let alertController = UIAlertController (title: "Title", message: "Go to Settings?", preferredStyle: .alert)
+            
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        print("Settings opened: \(success)") // Prints true
+                    })
+                }
+            }
+            alertController.addAction(settingsAction)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        //加入apple內建地圖的導航
+        } else if indexPath.row == 4 {
+            navigation(lat: 25.052288 , lon: 121.532258, placeName: "ALPHA Camp")
         }
     }
     
